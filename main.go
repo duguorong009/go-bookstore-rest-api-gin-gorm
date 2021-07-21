2b87f8bade2ae57a320dc0ac91a452c6369e0b08
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2021-07-21 11:41:29 -0400

    Implement the routing url "/books"
    Finish the gorm migration to sqlite3 impl.
    Implement the controller for REST API urls.
    First, implement GET "/books".

--- main.go
+++ main.go
@@ -4,14 +4,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"./controllers"
+	"./models"
 )
 
 func main() {
 	r := gin.Default()
 
+	models.ConnectDataBase()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello guorong"})
 	})
+
+	r.GET("/books", controllers.FindBooks)
 
 	r.Run()
 }
