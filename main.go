767c07a84127830a909644fbb7e8dd91d1bbbd2f
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2021-07-22 21:53:46 -0400

    Implement the PATCH "/book/:id"
    Implement the functionality of updating the book with id.

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 	r.GET("/books", controllers.FindBooks)
 	r.POST("/books", controllers.CreateBook)
 	r.GET("/books/:id", controllers.FindBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
 
 	r.Run()
 }
